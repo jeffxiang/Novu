e59162bdb26d710e080c7e1f535c11c5da49e1b6
Jun Han Jeff Xiang <jeffxiang@berkeley.edu>
2018-06-20 13:29:52 -0700

    improved logic flow for download_csv()

--- download.py
+++ download.py
@@ -1,7 +1,8 @@
-import gdax, requests, time
-from websocket import create_connection
+import gdax, time, threading
 
-# Get the most updated order book in .csv form
+# Author: Jeff Xiang
+
+# Get the most updated order book in .csv form.
 def write_csv(level):
     write_dir = "/Users/Jeff/Desktop/Novumind/ob_data_level3.csv"
     csv = open(write_dir, "w")
@@ -33,107 +34,74 @@ def write_csv(level):
     write_helper('bids')
     csv.close()
 
-def download_csv(filename, num_display=200, num_files=5):
+
+# Downloads the specified number of .csv files of the order book and saves as FILENAME.
+def download_csv(filename, num_files=10):
+    total_start_time = time.time()
     curr_file = 0
-    order_book = gdax.OrderBook(product_id='BTC-USD')
+    order_book = gdax.OrderBook(product_id="BTC-USD")
     order_book.start()
     while curr_file < num_files:
         file_dir = "/Users/Jeff/Desktop/Novumind/test_csv/" + filename + str(curr_file) + ".csv"
         csv = open(file_dir, "w")
         csv.truncate(0)
 
-        def write_helper(side, max_time=10):
-            is_asks = side is "asks"
-            t = time.clock()
-            while time.clock() - t < max_time:
-                book = order_book.get_current_book()
-                list = book[side]
-                if len(list) > 0:
-                    list_length = len(list)
-                    curr_index = list_length - 1
-                    if is_asks:
-                        curr_index = 0
-                    order_index = 0
-                    while (curr_index >= list_length - num_display and side is "bids") or (curr_index < num_display and side is "asks"):
-                        price = str(list[curr_index][0])
-                        size = str(list[curr_index][1])
-                        row = side + str(order_index) + "," + price + "," + size + "\n"
-                        csv.write(row)
-                        if is_asks:
-                            curr_index += 1
-                        else:
-                            curr_index -=1
-                        order_index += 1
-                    break
-
-        csv.write("side, price, size,\n")
-        write_helper("asks")
-        write_helper("bids")
+        t1 = time.time()
+        while True:
+            book = order_book.get_current_book()
+            book_timestamp = time.time()
+            if len(book["asks"]) > 0 and len(book["bids"]) > 0:
+                break
+        if curr_file == 0:
+            print("Time taken to obtain non-null order book: " + str(time.time() - t1))
+        csv.write("side, price, size, timestamp of order book: " + str(book_timestamp) + "\n")
+        t2 = time.time()
+        write_helper("asks", book, csv)
+        write_helper("bids", book, csv)
         csv.close()
+        write_time = time.time() - t2
+        print("Time taken to read order book & write file " + str(curr_file) + ": " + str(write_time))
         curr_file += 1
+        time.sleep(float(1 - write_time))
+    print("Time taken to write " + str(num_files) + " files: " + str(time.time() - total_start_time))
 
+# Helper function that writes the top NUM_DISPLAY orders on specified SIDE to CSV based on given BOOK.
+def write_helper(side, book, csv, num_display=200):
+    is_asks = side is "asks"
+    list = book[side]
+    list_length = len(list)
+    curr_index = list_length - 1
+    if is_asks:
+        curr_index = 0
+    order_index = 0
+    while (curr_index >= list_length - num_display and side is "bids") or (curr_index < num_display and side is "asks"):
+        price = str(list[curr_index][0])
+        size = str(list[curr_index][1])
+        row = side + str(order_index) + "," + price + "," + size + "\n"
+        csv.write(row)
+        if is_asks:
+            curr_index += 1
+        else:
+            curr_index -=1
+        order_index += 1
 
 def download_csv2(filename, num_display=200, num_files=10):
+    total_start_time = time.time()
     curr_file = 0
-    order_book = gdax.OrderBook(product_id='BTC-USD')
+    order_book = gdax.OrderBook(product_id="BTC-USD")
     order_book.start()
-    while curr_file < num_files:
-        file_dir = "/Users/Jeff/Desktop/Novumind/test_csv/" + filename + str(curr_file) + ".csv"
-        csv = open(file_dir, "w")
-        csv.truncate(0)
 
-        def write_helper(side, max_time=10):
-            is_asks = side is "asks"
-            t = time.clock()
-            while time.clock() - t < max_time:
-                book = order_book.get_current_book()
-                list = book[side]
-                if len(list) > 0:
-                    list_length = len(list)
-                    curr_index = list_length - 1
-                    if is_asks:
-                        curr_index = 0
-                    order_index = 0
-                    while (curr_index >= list_length - num_display and side is "bids") or (curr_index < num_display and side is "asks"):
-                        price = str(list[curr_index][0])
-                        size = str(list[curr_index][1])
-                        row = side + str(order_index) + "," + price + "," + size + "\n"
-                        csv.write(row)
-                        if is_asks:
-                            curr_index += 1
-                        else:
-                            curr_index -=1
-                        order_index += 1
-                    break
+    threading.Timer(1, get_order_book())
 
-        csv.write("side, price, size,\n")
-        write_helper("asks")
-        write_helper("bids")
-        csv.close()
-        curr_file += 1
-        time.sleep(1)
-
-
-def print_orders(side, max_time=10, num_display=200):
-    order_book = gdax.OrderBook(product_id='BTC-USD')
-    order_book.start()
-    t = time.clock()
-    print(side + ": ")
-    while time.clock() - t < max_time:
+def get_order_book():
+    order_book = gdax.OrderBook(product_id="BTC-USD")
+    while True:
         book = order_book.get_current_book()
-        list = book[side]
-        if len(list) > 0:
-            list_length = len(list)
-            curr_index = list_length - 1
-            order_index = 0
-            if side is "asks":
-                curr_index = 0
-            while (curr_index >= list_length - num_display and side is "bids") or (curr_index < num_display and side is "asks"):
-                print(str(order_index) + ": " + str(list[curr_index][0]) + ", " + str(list[curr_index][1]))
-                if side is "bids":
-                    curr_index -= 1
-                else:
-                    curr_index += 1
-                order_index += 1
+        book_timestamp = time.time()
+        if len(book["asks"]) > 0 and len(book["bids"]) > 0:
             break
-    order_book.close()
+    csv.write("side, price, size, timestamp of order book: " + str(book_timestamp) + "\n")
+    t2 = time.time()
+    write_helper("asks", book)
+    write_helper("bids", book)
+    csv.close()
